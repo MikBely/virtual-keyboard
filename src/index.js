12cb93f0a94f04e14febafb208a46bcba98907eb
Mikhail Bely <neverdream40@gmail.com>
2022-05-07 21:17:57 +0300

    feat: realize a class of button for keyboard

--- src/index.js
+++ src/index.js
@@ -1,5 +1,37 @@
-const a = 10;
-const b = a;
-const c = 'a';
-console.log(b);
-console.log(c);
+import keysID from './keysID';
+
+let num = 0;
+class KeyButton {
+  // constructor
+  constructor(nameOfClass, tagName) {
+    this.nameOfClass = nameOfClass;
+    this.id = keysID[num].key;
+    this.tagName = tagName;
+  }
+
+  // Create div of button
+  createButton() {
+    this.element = document.createElement('div');
+    return this.element;
+  }
+
+  // All buttons have the same className
+  addNameOfClass() {
+    this.element.className(this.nameOfClass);
+    return this.element;
+  }
+
+  addAttribute() {
+    this.element.setAttribute('data-name', this.id);
+    return this.element;
+  }
+
+  initButton() {
+    this.createButton();
+    this.addNameOfClass();
+    this.addAttribute();
+    this.element.textContent = `${keysID[num].en.lowercase}`;
+    num += 1;
+    return this.element;
+  }
+}
